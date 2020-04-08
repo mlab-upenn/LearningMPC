53b03673b1c549918ae008fdd9a3e1164d79d9b2
Yuwei Wang <wyw907@gmail.com>
2020-04-07 20:32:08 -0400

    learning tested

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuwei/yuwei_ws/src/LearningMPC
-# Build directory: /home/yuwei/yuwei_ws/src/LearningMPC/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/yuwei/yuwei_ws/src/LearningMPC/include".split(';') if "/home/yuwei/yuwei_ws/src/LearningMPC/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lLearningMPC".split(';') if "-lLearningMPC" != "" else []
-PROJECT_NAME = "LearningMPC"
-PROJECT_SPACE_DIR = "/home/yuwei/yuwei_ws/src/LearningMPC/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
